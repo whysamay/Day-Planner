1ac9dab3fa054524a9d264f0e70fd7b851f3d5a3
whysamay <samaypawar2200@gmail.com>
2025-12-12 21:53:02 +0530

    added prefix for auth

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 import models
 from database import engine
+from routers import auth, todos
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.include_router(auth.router, prefix="/auth")
+app.include_router(todos.router)

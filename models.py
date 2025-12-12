db6acafa694bd2272fd419228f211c9c158b8a4c
whysamay <samaypawar2200@gmail.com>
2025-12-12 21:52:29 +0530

    removed password and added hashed_password to User

--- models.py
+++ models.py
@@ -3,20 +3,20 @@ from database import Base
 
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 
-class Users(Base):
+class User(Base):
     __tablename__ = "users"
-    unique_id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     phone_number = Column(String, unique=True, index=True)
-    password = Column(String)
+    hashed_password = Column(String)
     role = Column(String)
 
 class Todo(Base):
-    __tablename__ = "todo"
+    __tablename__ = "todos"
 
-    unique_id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     description = Column(String)
     priority = Column(Integer)
-    owner_id = Column(Integer, ForeignKey("Users.unique_id"))
+    owner_id = Column(Integer, ForeignKey("users.id"))
     complete = Column(Boolean, default=False)
